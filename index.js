e20c2ca2faab2c7560bbdfb1345d60b76585dd7c
Dhiaa Alshamy <dhodhof@gmail.com>
2020-07-22 01:04:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
 fetch('https://anapioficeandfire.com/api/books').
 then(par=>par.json()).
-then(books=>renderBooks(books));
+then(json=>renderBooks(json));
 }
 
 function renderBooks(books) {
